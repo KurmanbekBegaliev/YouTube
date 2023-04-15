81bec320c62dd5d34f9f1fd92a7ab37bd276f9ec
KUBAdesctop <begishkuba@gmail.com>
2023-04-15 16:00:41 +0600

    Desctop commit -add ConnectivityObserver

--- app/src/main/java/com/example/youtube/MainActivity.kt
+++ app/src/main/java/com/example/youtube/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.youtube
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import com.example.youtube.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private  lateinit var binding : ActivityMainBinding
+    private lateinit var connectivityObserver: ConnectivityObserver
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContentView(binding.root)
+
+        connectivityObserver = NetworkConnectivityObserver(applicationContext)
+        if (connectivityObserver.observe().toString() != ConnectivityObserver.Status.Available.toString()) {
+            findNavController().navigate(R.id.noInternetFragment)
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/youtube/ui/nointernet/NoInternetViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.youtube.ui.nointernet
+
+import com.example.youtube.base.BaseViewModel
+
+class NoInternetViewModel: BaseViewModel() {
+}
